e5e7949fd51c5e9bd6e2849a2f24544c923d55a8
Thomas Nelson <thnlsn@gmail.com>
2022-01-18 19:48:59 -0800

    comments

--- src/App.js
+++ src/App.js
@@ -4,4 +4,7 @@ function App() {
   return <div className='App'>TEST TEST TEST</div>;
 }
 
+// App level will have header, content, footer
+// Content will have the choice screen for what kind of card
+
 export default App;
