28b188d52f0a34fe0bb96ee88f9df1b817263d5d
Thomas Nelson <thnlsn@gmail.com>
2022-02-26 22:45:49 -0800

    added blank image and fixed background size issue

--- src/components/CardEdit.jsx
+++ src/components/CardEdit.jsx
@@ -4,9 +4,11 @@ import { CardContext } from '../state/context';
 
 import TextInput from './Inputs/TextInput';
 import { getDimensions } from '../scripts/helpers';
+import blank from '../assets/images/blank.png';
 
 const CardEdit = () => {
-  const { cardType, name, changeName } = useContext(CardContext);
+  const { cardType, name, changeName, image, changeImage } =
+    useContext(CardContext);
 
   return (
     <div className='app-content card-editor-content'>
@@ -15,10 +17,15 @@ const CardEdit = () => {
         style={{
           width: getDimensions(cardType, 2).width,
           height: getDimensions(cardType, 2).height,
+          background: `url(${image}), url(${blank})`,
+          //backgroundSize: 'contain',
+          //objectFit: 'contain',
+          //backgroundSize: 'contain',
         }}
       ></div>
       <div className='card-form'>
         <TextInput label='name' state={name} changeState={changeName} />
+        <TextInput label='image' state={image} changeState={changeImage} />
       </div>
     </div>
   );
