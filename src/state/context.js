4525da4e896c55937586be9b0bb8d19d8ea910ad
Thomas Nelson <thnlsn@gmail.com>
2022-09-11 15:15:18 -0700

    context skeleton

--- src/state/context.js
+++ src/state/context.js
@@ -1,1 +1,24 @@
-import React, { useState, useCallback, createContext, useContext } from 'react'
+import React, { useState, useCallback, createContext, useContext } from 'react';
+import { initialState } from './initialState';
+
+const FourSouls = createContext(initialState);
+const FourSoulsContextProvider = ({ children }) => {
+    //~ //////////////
+    //* State ////////
+    //~ //////////////
+
+    //~ //////////////
+    //* Async ////////
+    //~ //////////////
+
+    const value = {};
+    return (
+        <FourSouls.Provider value={value}>{children}</FourSouls.Provider>
+    );
+}
+
+const useFourSoulsContext = () => {
+    return useContext(FourSouls);
+}
+
+export { FourSoulsContextProvider, useFourSoulsContext }
